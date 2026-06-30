bdb4e30cafd2f9345c3e04a0f86c77c097cdca2d
Aishwarya <aishwarya.thorat005@gmail.com>
2026-07-01 01:17:44 +0530

    persist shopping cart using localStorage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,8 +1,27 @@
 "use client";
-import { createContext, useState} from "react";
+import { createContext, useEffect, useState} from "react";
 export const CartContext = createContext();
 export default function CartProvider({ children}) {
     const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+
+        const savedCart = 
+    localStorage.getItem("cart");
+
+        if(savedCart) {
+            setCart(JSON.parse(savedCart));
+        }
+
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem(
+            "cart",
+            JSON.stringify(cart)
+        );
+
+    }, [cart]);
 
     return (
         <CartContext.Provider value={{ cart, setCart}}>
